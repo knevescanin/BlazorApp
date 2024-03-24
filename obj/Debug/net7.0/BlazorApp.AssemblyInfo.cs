f1ad45c1dbf951303f1b33048eb4e47813ef0d55
knevescanin <karlo.nevescanin21@gmail.com>
2024-03-24 21:31:25 +0100

    Register and login design

--- obj/Debug/net7.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd1d88a5930754e1e50869851a77a689004ec039")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000c960f54c2a8f389fc82e44db34ad51de51883")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
