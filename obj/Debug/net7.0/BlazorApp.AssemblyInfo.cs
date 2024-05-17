1b9967e9f8fd7d4d745b1477a0048ca92e5fe35a
knevescanin <karlo.nevescanin21@gmail.com>
2024-05-17 11:59:07 +0200

    main push

--- obj/Debug/net7.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bf44628d78204772d2512a06141c7569f80973c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a15059742c6aa7b4398b5613a6edf7884151fb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
